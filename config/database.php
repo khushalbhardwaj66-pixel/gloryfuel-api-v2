d18d1985e86c0d616e360523ebbc617be633bb63
khushalbhardwaj66-pixel <khushalbhardwaj66@gmail.com>
2026-04-25 16:47:54 +0530

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,29 @@
+<?php
+// config/database.php
+class Database {
+    private $host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+    private $db_name = "test";
+    private $username = "u8awf4fFprGi4MV.root";
+    private $password = "4Db2ddKV2qz8x5cl";
+    private $port = "4000";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $dsn = "mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name;
+            $this->conn = new PDO($dsn, $this->username, $this->password, [
+                PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
+            ]);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            header('Content-Type: application/json');
+            echo json_encode(["error" => "Connection error: " . $exception->getMessage()]);
+            exit;
+        }
+        return $this->conn;
+    }
+}
+?>
